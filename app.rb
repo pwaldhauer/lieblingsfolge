e4f23e66d65ef08790803e639fa4201b46fb8d06
Philipp Waldhauer <ich@philippwaldhauer.de>
2013-02-11 19:52:26 +0100

    Test

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require 'sinatra'
 
 require 'sinatra-twitter-oauth'
 
+require 'rss'
+require 'open-uri'
+
 require './models.rb'
 
 set :twitter_oauth_config,  :key => '2zOCcvGwwJDqNxOG1DDa6A',
@@ -37,3 +40,36 @@ get '/vote' do
 
     redirect '/'
 end
+
+get '/update' do
+
+  @regexp = /28_(\d+): (.*)/
+
+  url = 'http://konferenz28.de/rss'
+  open(url) do |rss|
+    feed = RSS::Parser.parse(rss)
+    feed.items.each do |item|
+
+      @split = @regexp.match(item.title);
+    
+      puts @split[1] + " - " + @split[2]
+
+      @folge = Folge.first(:nummer => @split[1].to_i)
+
+      puts @folge
+      if @folge != nil then
+          next
+      end
+
+      @folge = Folge.new
+      @folge.link = item.link
+      @folge.nummer = @split[1].to_i
+      @folge.title = @split[2]
+      @folge.save
+
+    end
+  end
+
+  redirect '/'
+end
+
